99d3c6e96825d91fca11422244a05ffd68a943f7
Willian Ribeiro <64652475+Willian-R@users.noreply.github.com>
2020-11-22 21:08:15 -0300

    Debian stable para testing

---
+++ bullseye.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo deb http://deb.debian.org/debian bullseye main > /etc/apt/sources.list
+echo deb http://deb.debian.org/debian bullseye-updates main >> /etc/apt/sources.list
+sudo apt update
+sudo apt full-upgrade -y
+sudo apt autoclean && sudo apt autoremove -y
