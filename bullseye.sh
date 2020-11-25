8cc20e60b987d13b0e7b023f8c4915ee78f7d980
Willian Ribeiro <64652475+Willian-R@users.noreply.github.com>
2020-11-25 17:27:58 -0300

    Update bullseye.sh

--- bullseye.sh
+++ bullseye.sh
@@ -5,3 +5,6 @@ sudo apt update
 sudo apt full-upgrade -y
 sudo apt autoclean && sudo apt autoremove -y
 systemctl reboot
+sudo apt remove xfburn xfce4-sensors-plugin mousepad xfce4-taskmanager xfce4-clipman xfce4-screenshooter atril libreoffice-common xfce4-dict xsane hv3 parole quodlibet gcc-8-base exfalso
+sudo apt autoclean && sudo apt autoremove -y
+sudo apt install vlc transmission-gtk papirus-icon-theme
