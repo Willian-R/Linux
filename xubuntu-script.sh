59cf6f31baca8093db8cf97ed99d4c51d75036f7
Willian Ribeiro <64652475+Willian-R@users.noreply.github.com>
2020-11-24 23:03:43 -0300

    Update xubuntu-script.sh

--- xubuntu-script.sh
+++ xubuntu-script.sh
@@ -7,28 +7,28 @@ echo
 # atualizar o sistema
 echo "Atualizando o sistema"
 echo "====================="
-sleep 2
+sleep 1
 sudo apt update && sudo apt upgrade -y
 echo
 
 # removendo os programas padrão do ubuntu
 echo "Removendo os programas desnecessários"
 echo "====================================="
-sleep 2
+sleep 1
 sudo apt remove xfce4-taskmanager gnome-software-common xfburn onboard-common xfce4-notes xfce4-notes-plugin mousepad xfce4-screenshooter mate-calc-common atril-common libreoffice-common xfce4-dict simple-scan gimp gimp-data gimp-help-common pidgin pidgin-data pidgin-otr thunderbird thunderbird-locale-en thunderbird-locale-en-us thunderbird-locale-pt thunderbird-locale-pt-br sgt-launcher gnome-mines gnome-sudoku parole gigolo -y
 echo
 
 # limpando os pacotes órfãos
 echo "limpando os pacotes"
 echo "==================="
-sleep 2
+sleep 1
 sudo apt autoclean && sudo apt autoremove -y
 echo
 
 #instalando programas
 echo "Instalando programas"
 echo "==================="
-sleep 2
+sleep 1
 sudo apt install vlc htop papirus-icon-theme neofetch -y
 clear
 neofetch
