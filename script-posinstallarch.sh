a81968c8d859e0a70411a6affa0f716244b199b1
Willian Ribeiro <64652475+Willian-R@users.noreply.github.com>
2020-11-28 00:53:57 -0300

    Update script-posinstallarch.sh

--- script-posinstallarch.sh
+++ script-posinstallarch.sh
@@ -15,7 +15,7 @@ useradd -m -g users -G wheel -s /bin/bash ${usuario}
 read -p "Digite a senha para o usuário: " senhausuario
 echo "${usuario}:${senhausuario}" | chpasswd
 echo "${usuario} ALL=(ALL) ALL" >> /etc/sudoers
-pacman -S xorg xfce4 xfce4-goodies xdg-user-dirs gvfs lightdm lightdm-gtk-greeter firefox vlc gtkmm3 xf86-video-vmware xf86-input-vmmouse open-vm-tools papirus-icon-theme networkmanager network-manager-applet pulseaudio --noconfirm
+pacman -S xorg xfce4 xfce4-goodies xdg-user-dirs gvfs lightdm lightdm-gtk-greeter firefox vlc gtkmm3 xf86-video-vmware xf86-input-vmmouse open-vm-tools papirus-icon-theme networkmanager network-manager-applet pulseaudio bash-completion htop --noconfirm
 grub-install "${ESCOLHA}"
 systemctl enable lightdm
 systemctl enable vmtoolsd
