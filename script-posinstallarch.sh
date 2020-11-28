f535526fdde7379d1c2603e59bb2f4ef43fa3891
Willian Ribeiro <64652475+Willian-R@users.noreply.github.com>
2020-11-28 01:19:18 -0300

    Update script-posinstallarch.sh

--- script-posinstallarch.sh
+++ script-posinstallarch.sh
@@ -18,6 +18,7 @@ echo "${usuario} ALL=(ALL) ALL" >> /etc/sudoers
 pacman -S xorg xfce4 xfce4-goodies xdg-user-dirs gvfs lightdm lightdm-gtk-greeter firefox vlc gtkmm3 xf86-video-vmware xf86-input-vmmouse open-vm-tools papirus-icon-theme networkmanager network-manager-applet pulseaudio bash-completion htop --noconfirm
 pacman -Syu --noconfirm
 pacman -R xfburn xfce4-sensors-plugin xfce4-notes-plugin mousepad orage xfce4-taskmanager xfce4-clipman-plugin xfce4-screenshooter xfce4-dict parole --noconfirm
+pacman -Scc --noconfirm
 read "Digite o dispositivo para instalação do GRUB: " ESCOLHA
 grub-install ${ESCOLHA}
 systemctl enable lightdm
